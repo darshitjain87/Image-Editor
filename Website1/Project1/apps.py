47c1a5bf84bbcd4020e894fb9506ba4df3cb2835
darshitjain87 <94097200+darshitjain87@users.noreply.github.com>
2023-03-11 16:36:36 +0530

    Add files via upload

---
+++ Website1/Project1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Project1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Project1'
---
+++ Website1/Project1/bg.py
@@ -0,0 +1,60 @@
+import os
+import cv2
+import numpy as np
+import mediapipe as mp
+
+def BGRemover():
+    mp_selfie_segmentation = mp.solutions.selfie_segmentation
+    selfie_segmentation = mp_selfie_segmentation.SelfieSegmentation(model_selection=1)
+
+    image_path = "C:/Users/DELL/Desktop/D/images"
+    images = os.listdir(image_path)
+
+    image_index= 0
+    bg_image = cv2.imread(image_path+'/'+images[image_index])
+
+    cap = cv2.VideoCapture(0)
+    while cap.isOpened():
+        _, input_image = cap.read()
+        if not _:
+            break
+
+        # flip the input_image to horizontal direction
+        input_image = cv2.flip(input_image, 1)
+        height , width, channel = input_image.shape
+
+        RGB = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
+
+        # get the result 
+        results = selfie_segmentation.process(RGB)
+
+        # extract segmented mask
+        mask = results.segmentation_mask
+
+        # it returns true or false where the condition applies in the mask
+        condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.6
+
+        # resize the background image to the same size of the original input_image
+        bg_image = cv2.resize(bg_image, (width, height))
+
+        # combine input_image and background image using the condition
+        output_image = np.where(condition, input_image, bg_image)
+
+        cv2.putText(input_image, "Press b to change, s to save, q to quit", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+        cv2.imshow("input_image", input_image)
+        cv2.imshow("Output", output_image)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+        elif key == ord('s'):
+            cv2.imwrite("C:/Users/DELL/Pictures/saved/input.png",input_image)
+            cv2.imwrite("C:/Users/DELL/Pictures/saved/bg.png",bg_image)
+            cv2.imwrite("C:/Users/DELL/Pictures/saved/output.png",output_image)
+        elif key == ord('b'):
+            if image_index != len(images)-1:
+                image_index += 1
+            else:
+                image_index = 0
+            bg_image = cv2.imread(image_path+'/'+images[image_index])
+    cap.release()
+    cv2.destroyAllWindows()
