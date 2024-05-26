f57f4e577cabe2dd663f6813a5861d790222daf9
darshitjain87 <94097200+darshitjain87@users.noreply.github.com>
2024-05-26 16:48:51 +0530

    Update settings.py

--- Website1/Website1/settings.py
+++ Website1/Website1/settings.py
@@ -123,6 +123,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_dir')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
